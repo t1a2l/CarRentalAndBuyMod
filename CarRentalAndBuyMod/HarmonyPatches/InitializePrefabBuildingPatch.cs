7d22921cdbb0302de6bf87a9bc26331a8b529c74
Tal <talrofeh@gmail.com>
2024-05-06 21:42:36 +0300

    convert gas station

---
+++ CarRentalAndBuyMod/HarmonyPatches/InitializePrefabBuildingPatch.cs
@@ -0,0 +1,32 @@
+using System;
+using HarmonyLib;
+using Object = UnityEngine.Object;
+using CarRentalAndBuyMod.AI;
+using CarRentalAndBuyMod.Utils;
+using UnityEngine;
+
+namespace CarRentalAndBuyMod.HarmonyPatches
+{
+    [HarmonyPatch(typeof(BuildingInfo), "InitializePrefab")]
+    public static class InitializePrefabBuildingPatch
+    {
+        public static void Prefix(BuildingInfo __instance)
+        {
+            try
+            {
+                if ((__instance.name.ToLower().Contains("gas station") || __instance.name.ToLower().Contains("gasstation") || __instance.name.ToLower().Contains("gaspumps")) && __instance.GetAI() is not GasStationAI)
+                {
+                    var oldAI = __instance.GetComponent<PrefabAI>();
+                    Object.DestroyImmediate(oldAI);
+                    var newAI = (PrefabAI)__instance.gameObject.AddComponent<GasStationAI>();
+                    PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.ToString());
+            }
+        }
+
+    }
+}
